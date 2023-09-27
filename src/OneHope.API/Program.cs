5fc2bfff08a9b1b065b40b9ecbb6860fb2a7919b
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-09-27 12:36:15 +0200

    Add a service for managing a sqlserver database that will be managed using ApplicationDBContext the connection to the database was defined in appsettings

--- src/OneHope.API/Program.cs
+++ src/OneHope.API/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using OneHope.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add service for managing a sqlserver database that will be managed using ApplicationDBContext
+// the connection to the database was defined in appsettings
+builder.Services.AddDbContext<ApplicationDBContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
