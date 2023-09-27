dd26681d97722526e077e28cb2604d9b43cfc9fa
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-09-27 12:31:17 +0200

    Create Models Folder.

---
+++ src/OneHope.API/Models/ApplicationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace OneHope.API.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+                : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
+
