1a88fddf31d8d7292e2970d768155104ff796b38
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-10-04 12:35:12 +0200

    Se ha creado la clase Portatil con sus atributos y constructores de forma básica (falta especificar bien los atributos y conocer los constructores que necesitamos bien) #10

---
+++ src/OneHope.API/Models/Portatil.cs
@@ -0,0 +1,37 @@
+﻿namespace OneHope.API.Models
+{
+    public class Portatil
+    {
+        //Atributos
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public float PrecioCompra { get; set; }
+        [Required]
+        public float PrecioAlq { get; set;}
+        [Required]
+        public float PrecioCoste { get; set; }
+        [Required]
+        public int Stock {  get; set; }
+        [Required]
+        public int StockAlq { get; set; }
+
+        public int Proveedor { get; set;}
+        //Constructores
+        public Portatil() { }
+
+        public Portatil(int iD, string modelo, float precioCompra, float precioAlq, float precioCoste, int stock, int stockAlq, int proveedor)
+        {
+            ID = iD;
+            Modelo = modelo;
+            PrecioCompra = precioCompra;
+            PrecioAlq = precioAlq;
+            PrecioCoste = precioCoste;
+            Stock = stock;
+            StockAlq = stockAlq;
+            Proveedor = proveedor;
+        }
+    }
+}
