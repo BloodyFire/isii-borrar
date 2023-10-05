d6751b525bf76a9816fda202faeb1d2306e553e2
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-10-05 13:19:51 +0200

    Se ha creado la clase Alquiler con diferentes atributos (faltan restricciones y hacer referencia a las claves foraneas a las que pertenece) #10

---
+++ src/OneHope.API/Models/Alquiler.cs
@@ -0,0 +1,26 @@
+﻿namespace OneHope.API.Models
+{
+    public class Alquiler
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required, DataType(DataType.Date), Display(Name ="Fecha de Alquiler")]
+        [DisplayFormat(DataFormatString = "dd/MM/yyyy", ApplyFormatInEditMode = true)]
+        public DateTime FechaAlquiler { get; set; }
+
+        [Required, DataType(DataType.Date), Display(Name = "Fecha Inicio de Alquiler")]
+        [DisplayFormat(DataFormatString = "dd/MM/yyyy", ApplyFormatInEditMode = true)]
+        public DateTime FechaInAlquiler { get; set; }
+
+        [Required, DataType(DataType.Date), Display(Name = "Fecha Fin de Alquiler")]
+        [DisplayFormat(DataFormatString = "dd/MM/yyyy", ApplyFormatInEditMode = true)]
+        public DateTime FechaFinAlquiler { get; set; }
+        
+        public int Total { get; set; }
+
+        [Required, StringLength(75, ErrorMessage = "La calle no puede ser mayor de 75 caracteres.")]
+        [RegularExpression(@"[a-ZA-Z] + [a-zA-Z\s]*$")]
+        public string DireccionEnvio { get; set; }
+    }
+}
