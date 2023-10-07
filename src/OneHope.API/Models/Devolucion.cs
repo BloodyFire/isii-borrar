6a2f202a4b7a4121607352e453a4a95abdb9d8f1
PAULA ORTEGA LÓPEZ <Paula.Ortega4@alu.uclm.es>
2023-10-07 10:47:10 +0200

    Creación de la clase devolución #11

---
+++ src/OneHope.API/Models/Devolucion.cs
@@ -0,0 +1,33 @@
+﻿namespace OneHope.API.Models
+{
+    public class Devolucion
+    {
+
+        public Devolucion() { }
+
+        [Key]
+        public int IdDevolucion { get; set; }
+
+        public int IdCompra { get; set; }
+
+        [DataType(DataType.Date), Display(Name = "Release Date")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha { get; set; }
+
+        public float Total { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Devolucion devolucion &&
+                   IdDevolucion == devolucion.IdDevolucion &&
+                   IdCompra == devolucion.IdCompra &&
+                   Fecha == devolucion.Fecha &&
+                   Total == devolucion.Total;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(IdDevolucion, IdCompra, Fecha, Total);
+        }
+    }
+}
