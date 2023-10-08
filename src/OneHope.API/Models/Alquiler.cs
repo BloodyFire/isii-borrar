eb34e0ba4c567f96d8ad5a6bfd65d8bf173a3104
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-10-08 23:55:17 +0200

    Se han cambiado tipos y restricciones de los atributos, se han creado los constructores, los metodos Equals y Hash y se ha añadido el Metodo de pago. Falta hacer la relacion con la clase LineaAlquiler. #10

--- src/OneHope.API/Models/Alquiler.cs
+++ src/OneHope.API/Models/Alquiler.cs
@@ -2,25 +2,68 @@
 {
     public class Alquiler
     {
+        //Atributos
         [Key]
         public int ID { get; set; }
 
         [Required, DataType(DataType.Date), Display(Name ="Fecha de Alquiler")]
-        [DisplayFormat(DataFormatString = "dd/MM/yyyy", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "0:dd/MM/yyyy", ApplyFormatInEditMode = true)]
         public DateTime FechaAlquiler { get; set; }
 
         [Required, DataType(DataType.Date), Display(Name = "Fecha Inicio de Alquiler")]
-        [DisplayFormat(DataFormatString = "dd/MM/yyyy", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "0:dd/MM/yyyy", ApplyFormatInEditMode = true)]
         public DateTime FechaInAlquiler { get; set; }
 
         [Required, DataType(DataType.Date), Display(Name = "Fecha Fin de Alquiler")]
-        [DisplayFormat(DataFormatString = "dd/MM/yyyy", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "0:dd/MM/yyyy", ApplyFormatInEditMode = true)]
         public DateTime FechaFinAlquiler { get; set; }
-        
-        public int Total { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public float Total { get; set; }
 
         [Required, StringLength(75, ErrorMessage = "La calle no puede ser mayor de 75 caracteres.")]
         [RegularExpression(@"[a-zA-Z\s]*$")]
         public string DireccionEnvio { get; set; }
+
+        [Required]
+        [Display(Name = "Metodo de Pago")]
+        public MetodoPago MetodoPago { get; set; }
+
+        //Constructores
+        public Alquiler() 
+        {
+
+        }
+
+        public Alquiler(int ID, DateTime FechaAlquiler, DateTime FechaInAlquiler, DateTime FechaFinAlquiler, float Total, string DireccionEnvio, MetodoPago MetodoPago)
+        {
+            this.ID = ID;
+            this.FechaAlquiler = FechaAlquiler;
+            this.FechaInAlquiler = FechaInAlquiler;
+            this.FechaFinAlquiler = FechaFinAlquiler;
+            this.Total = Total;
+            this.DireccionEnvio = DireccionEnvio;
+            this.MetodoPago = MetodoPago;
+
+        }
+
+        //Metodos
+        public override bool Equals(object? obj)
+        {
+            return obj is Alquiler alq &&
+                   ID == alq.ID &&
+                   Total == alq.Total &&
+                   FechaAlquiler == alq.FechaAlquiler &&
+                   FechaInAlquiler == alq.FechaInAlquiler &&
+                   FechaFinAlquiler == alq.FechaFinAlquiler &&
+                   DireccionEnvio == alq.DireccionEnvio &&
+                   EqualityComparer<MetodoPago>.Default.Equals(MetodoPago, alq.MetodoPago);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID, Total, FechaAlquiler, FechaInAlquiler, FechaFinAlquiler, DireccionEnvio, MetodoPago);
+        }
     }
 }
