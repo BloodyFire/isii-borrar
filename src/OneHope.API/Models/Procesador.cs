bbfd37150ff11c34aa2d0c2f11b088909fa79908
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-10-08 22:27:59 +0200

    Se ha creado la clase Procesador con sus constructores, atributos y los metodos Equals y Hash. Falta hacer la relacion mediante una FK a la clase Portatil para que pueda ser un atributo de la misma. #10

---
+++ src/OneHope.API/Models/Procesador.cs
@@ -0,0 +1,33 @@
+﻿namespace OneHope.API.Models
+{
+    public class Procesador
+    {
+        //Constructores
+        public Procesador() { }
+
+        public Procesador(int ID, string Nombre) 
+        {
+            this.ID = ID;
+            this.Nombre = Nombre;
+        }
+
+        //Atributos
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(30, ErrorMessage = "El procesador no puede tener mas de 30 caracteres.")]
+        public string Nombre { get; set;}
+
+        //Metodos
+        public override bool Equals(object? obj)
+        {
+            return obj is Procesador proc && ID == proc.ID &&
+                   Nombre == proc.Nombre;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID, Nombre);
+        }
+    }
+}
