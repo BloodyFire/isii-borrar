f98aecb222bf13da977c52dd680bc3cc644fcadb
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-10-09 00:18:32 +0200

    Se han añadido atributos, restricciones, constructores y los metodos Equals y Hash. Falta hacer la relacion con Alquiler y Portatil. #10

--- src/OneHope.API/Models/LineaAlquiler.cs
+++ src/OneHope.API/Models/LineaAlquiler.cs
@@ -2,12 +2,43 @@
 {
     public class LineaAlquiler
     {
+        //Atributos
         [Key]
         public int ID { get; set; }
 
         public int PortatilID { get; set; }
+        public float PortatilPrecioAlquiler { get; set; }
+
+
         public int AlquilerID { get; set; }
 
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad minima para alquilar es 1.")]
         public int Cantidad { get; set; }
+
+        //Constructores
+        public LineaAlquiler() { }
+
+        public LineaAlquiler(int ID, int Cantidad, float PortatilPrecioAlquiler)
+        {
+            this.ID = ID;
+            this.Cantidad = Cantidad;
+            this.PortatilPrecioAlquiler = PortatilPrecioAlquiler;
+        }
+
+        //Metodos
+        public override bool Equals(object? obj)
+        {
+            return obj is LineaAlquiler item &&
+                   PortatilID == item.PortatilID &&
+                   AlquilerID == item.AlquilerID &&
+                   Cantidad == item.Cantidad &&
+                   PortatilPrecioAlquiler == item.PortatilPrecioAlquiler;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PortatilID, AlquilerID, Cantidad, PortatilPrecioAlquiler);
+        }
     }
 }
