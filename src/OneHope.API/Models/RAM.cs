4c9e5047de3885108e146a7d61e4dffa8d779780
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-10-08 22:32:28 +0200

    Se ha creado la clase RAM con sus constructores, atributos y los metodos Equals y Hash. Falta hacer la relacion mediante una FK a la clase Portatil para que pueda ser un atributo de la misma. #10

---
+++ src/OneHope.API/Models/RAM.cs
@@ -0,0 +1,33 @@
+﻿namespace OneHope.API.Models
+{
+    public class RAM
+    {
+        //Constructores
+        public RAM() { }
+        public RAM(int ID, string Nombre) 
+        {
+            this.ID = ID;
+            this.Nombre = Nombre;
+        }
+
+        //Atributos
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(25, ErrorMessage = "La RAM no puede ser mayor a 25 caracteres.")]
+        public string Nombre { get; set;}
+
+        //Metodos
+        public override bool Equals(object? obj)
+        {
+            return obj is RAM rAM && ID == rAM.ID &&
+                   Nombre == rAM.Nombre;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID, Nombre);
+        }
+    }
+}
