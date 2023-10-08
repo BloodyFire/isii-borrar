3663ffb1e6b8a2bd85bb18e117c02a420f59dc0c
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-10-09 01:13:40 +0200

    Se han añadido los datos del cliente en la clase Alquiler como atributos de la misma y se ha añadido que los metodos de pago sea de tipo enum. #10

--- src/OneHope.API/Models/Alquiler.cs
+++ src/OneHope.API/Models/Alquiler.cs
@@ -1,4 +1,6 @@
-﻿namespace OneHope.API.Models
+﻿using System.Numerics;
+
+namespace OneHope.API.Models
 {
     public class Alquiler
     {
@@ -22,9 +24,23 @@
         [DataType(DataType.Currency)]
         public float Total { get; set; }
 
+        [Required, StringLength(50, ErrorMessage = "El cliente no puede tener un nombre que supere los 50 caracteres.")]
+        [RegularExpression(@"[a-zA-Z]*$")]
+        public string NombreCliente { get; set; }
+
+        [Required, StringLength(50, ErrorMessage = "El cliente no puede tener unos Apellidos que supere los 50 caracteres.")]
+        [RegularExpression(@"[a-zA-Z\s]*$")]
+        public string ApellidosCliente { get; set; }
+
         [Required, StringLength(75, ErrorMessage = "La calle no puede ser mayor de 75 caracteres.")]
         [RegularExpression(@"[a-zA-Z\s]*$")]
         public string DireccionEnvio { get; set; }
+
+        [Required, EmailAddress]
+        public string EmailCliente { get; set; }
+
+        [Phone]
+        public int? TelefonoCliente { get; set; }
 
         [Required]
         [Display(Name = "Metodo de Pago")]
@@ -38,7 +54,7 @@
             LineasAlquiler = new List<LineaAlquiler>();
         }
 
-        public Alquiler(int ID, DateTime FechaAlquiler, DateTime FechaInAlquiler, DateTime FechaFinAlquiler, float Total, string DireccionEnvio, MetodoPago MetodoPago, IList<LineaAlquiler> LineasAlquiler)
+        public Alquiler(int ID, DateTime FechaAlquiler, DateTime FechaInAlquiler, DateTime FechaFinAlquiler, float Total, string NombreCliente, string ApellidosCliente, string DireccionEnvio, string EmailCliente, int TelefonoCliente, MetodoPago MetodoPago, IList<LineaAlquiler> LineasAlquiler)
         {
             this.ID = ID;
             this.FechaAlquiler = FechaAlquiler;
@@ -46,6 +62,10 @@
             this.FechaFinAlquiler = FechaFinAlquiler;
             this.Total = Total;
             this.DireccionEnvio = DireccionEnvio;
+            this.EmailCliente = EmailCliente;
+            this.NombreCliente = NombreCliente;
+            this.ApellidosCliente = ApellidosCliente;
+            this.TelefonoCliente = TelefonoCliente;
             this.MetodoPago = MetodoPago;
             this.LineasAlquiler = LineasAlquiler;
 
@@ -68,6 +88,14 @@
         public override int GetHashCode()
         {
             return HashCode.Combine(ID, Total, FechaAlquiler, FechaInAlquiler, FechaFinAlquiler, DireccionEnvio, MetodoPago, LineasAlquiler);
+        }
+
+        //Tipo de metodo de pago
+        public enum MetodPago
+        {
+            TarjetaCredito,
+            PayPal,
+            Transferencia
         }
     }
 }
