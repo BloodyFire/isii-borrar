243b8336ab6656c82b26f696d790117e3cce39cc
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-10-08 12:41:24 +0200

    He eliminado las clases LineaPedido y Pedido que no pertenecen a este CU.#9

--- src/OneHope.API/Models/ApplicationDBContext.cs
+++ src/OneHope.API/Models/ApplicationDBContext.cs
@@ -14,12 +14,10 @@ namespace OneHope.API.Models
 
             builder.Entity<Linea_Compra>().HasAlternateKey(pi => new { pi.Id_Prod, pi.Id_Compra });
 
-            builder.Entity<Linea_Pedido>().HasAlternateKey(pi => new { pi.Id_Prod, pi.Id_Linea });
         }
 
         public DbSet<Compra> Compras { get; set; }
         public DbSet<Portatil> Portatiles { get; set; }
-        public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Ram> Rams { get; set; }
         public DbSet<Procesador> Procesadores { get; set; }
         public DbSet<Marca> Marcas { get; set; }
