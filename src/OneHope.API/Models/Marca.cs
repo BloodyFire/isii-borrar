ae574ff8e7a44afa59ee3e9ccbdd0006d0d94ea8
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-10-08 22:20:35 +0200

    Se ha creado la clase Marca con sus constructores, atributos y los metodos Equals y Hash. Falta hacer la relacion mediante una FK a la clase Portatil para que pueda ser un atributo de la misma. #10

---
+++ src/OneHope.API/Models/Marca.cs
@@ -0,0 +1,32 @@
+﻿namespace OneHope.API.Models
+{
+    public class Marca
+    {
+        //Constructores
+        public Marca() { }
+
+        public Marca(int ID, string Nombre) 
+        {
+        }
+
+        //Atributos
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(75, ErrorMessage = "La marca no puede tener mas de 75 caracteres.")]
+        public string Nombre { get; set;}
+
+        //Metodos
+        public override bool Equals(object? obj)
+        {
+            return obj is Marca proc && ID == proc.ID &&
+                Nombre == proc.Nombre;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID, Nombre);
+        }
+
+    }
+}
