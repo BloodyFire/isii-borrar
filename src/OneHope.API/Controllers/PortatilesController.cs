c79f793f4c86c9fb953203ad59d825f6b6cba9be
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-10-25 14:07:53 +0200

    Acabo de crear los controladores de Portátiles y Pagos, al igual que he declarado el _context y el _logger. #30

---
+++ src/OneHope.API/Controllers/PortatilesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OneHope.API.Models;
+
+namespace OneHope.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PortatilesController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+
+        private readonly ILogger<PortatilesController> _logger;
+    }
+}
