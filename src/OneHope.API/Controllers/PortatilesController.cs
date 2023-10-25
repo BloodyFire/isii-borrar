406a9a1650595604f93ea715a9bd74e5a75bb2c3
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-10-25 16:21:37 +0200

    He creado la task "GetPortatilesParaComprar" siguiendo los filtros que se daban en el CU-Comprar Portátiles.#30

--- src/OneHope.API/Controllers/PortatilesController.cs
+++ src/OneHope.API/Controllers/PortatilesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using OneHope.API.Models;
 using System.Net;
 
@@ -33,6 +34,27 @@ namespace OneHope.API.Controllers
                 _logger.LogError($"{DateTime.Now} Critical error: proceso demasiado complejo.");
                 return BadRequest();
             }
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Portatil>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Portatil>> GetPortatilesParaComprar(string? nombre,
+            string? marca, string? procesador, string? ram, int? precio)
+        {
+            var portatiles = await _context.Portatiles
+                .Where(portatil => (nombre == null || portatil.Nombre.Contains(nombre)) &&
+                      (marca == null || portatil.Marca.NombreMarca.Equals(marca)) &&
+                      (procesador == null || portatil.Procesador.ModeloProcesador.Equals(procesador)) &&
+                      (ram == null || portatil.Ram.Capacidad.Equals(ram)) &&
+                      (precio == null || portatil.PrecioCompra.Equals(precio)) &&
+                      (portatil.Stock > 0))
+                .Include(Portatil => Portatil.Marca)
+                .Include(Portatil => Portatil.Procesador)
+                .Include(Portatil => Portatil.Ram)
+                .ToListAsync();
+
+            return Ok(portatiles);
         }
     }
 }
