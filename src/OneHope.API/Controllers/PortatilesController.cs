622f8d2b6141a3a67a463582312eb4548244a635
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-10-25 14:25:38 +0200

    He creado la Task "GetComputingProcess" en el controlador de PortatilesController.#30

--- src/OneHope.API/Controllers/PortatilesController.cs
+++ src/OneHope.API/Controllers/PortatilesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OneHope.API.Models;
+using System.Net;
 
 namespace OneHope.API.Controllers
 {
@@ -11,5 +12,27 @@ namespace OneHope.API.Controllers
         private readonly ApplicationDBContext _context;
 
         private readonly ILogger<PortatilesController> _logger;
+
+        public PortatilesController(ApplicationDBContext context, ILogger<PortatilesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<string>> GetComputingProcess(bool error)
+        {
+            if (!error) {
+                string result = "Este es el resultado de un proceso complejo.";
+                return Ok(result);
+            }
+            else
+            {
+                _logger.LogError($"{DateTime.Now} Critical error: proceso demasiado complejo.");
+                return BadRequest();
+            }
+        }
     }
 }
