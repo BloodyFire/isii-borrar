36bd066bc7588c217b82faa6a3bea331373baefb
DANIEL TOMÁS MARTÍNEZ <Daniel.Tomas@alu.uclm.es>
2023-10-31 20:10:59 +0100

    Creado fichero OneHope4SqliteUT.cs para realizar consultas sqlite y añadidos namespaces al global using. #28

--- test/OneHope.UT/GlobalUsings.cs
+++ test/OneHope.UT/GlobalUsings.cs
@@ -1,1 +1,7 @@
 global using Xunit;
+global using OneHope;
+global using OneHope.API.Models;
+global using OneHope.API.Controllers;
+global using OneHope.Shared.PortatilDTOs;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.Extensions.Logging;
