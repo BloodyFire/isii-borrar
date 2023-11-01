4db9e14c5a3052d42ee49b7f8c895219cbde42f8
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-11-01 20:51:34 +0100

    He creado un DTO para los detalles de la compra. Lo he nombrado DetallesCompraDTO.#30

---
+++ src/OneHope.Shared/CompraDTOs/DetallesCompraDTO.cs
@@ -0,0 +1,45 @@
+﻿using OneHope.Design.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OneHope.Shared.CompraDTOs
+{
+    public class DetallesCompraDTO : CompraPorCrearDTO
+    {
+        public DetallesCompraDTO() { }
+
+        public DetallesCompraDTO(int id, string nombreUsuario, string apellidosUsuario, 
+            string direccion, IList<CompraPortatilDTO> portatilesComprados, TipoMetodoPago metodoPago,
+            DateTime fechaCompra) : 
+            base(direccion, portatilesComprados, nombreUsuario, apellidosUsuario, metodoPago)
+            {
+                id = id;
+                fechaCompra = fechaCompra;
+            }
+
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("fechaCompra")]
+        public DateTime FechaCompra { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DetallesCompraDTO dTO &&
+                   base.Equals(obj) &&
+                   direccion == dTO.direccion &&
+                   EqualityComparer<IList<CompraPortatilDTO>>.Default.Equals(compraPortatiles, dTO.compraPortatiles) &&
+                   PrecioTotal == dTO.PrecioTotal &&
+                   nombreUsuario == dTO.nombreUsuario &&
+                   apellidosUsuario == dTO.apellidosUsuario &&
+                   MetodoPago == dTO.MetodoPago &&
+                   Id == dTO.Id &&
+                   FechaCompra == dTO.FechaCompra;
+        }
+    }
+}
