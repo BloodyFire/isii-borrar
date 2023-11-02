e9711e94a4cfe4920f3b0c85c51a70a3454d6840
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-11-02 20:06:22 +0100

    He añadido los métodos de pago al Shared para poder realizar el controlador de las compras.#30

---
+++ src/OneHope.Shared/TiposMetodoPago.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneHope.Shared
+{
+    public enum TiposMetodoPago
+    {
+        TarjetaCredito,
+        PayPal,
+        Transferencia
+    }
+}
