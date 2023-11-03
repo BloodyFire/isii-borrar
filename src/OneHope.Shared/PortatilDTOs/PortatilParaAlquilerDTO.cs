5d2fd9c0e16d454626bbcd2edf77861e7f142259
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-11-03 20:41:24 +0100

    Creación del DTO para el Alquiler de portatiles. Añadidos metodos hash, equals, creados los atributos necesarios y un constructor con dichos atributos. #29

---
+++ src/OneHope.Shared/PortatilDTOs/PortatilParaAlquilerDTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneHope.Shared.PortatilDTOs
+{
+    public class PortatilParaAlquilerDTO
+    {
+        public PortatilParaAlquilerDTO(int id, string nombre, string marca, string procesador, int cantidad, double precioAlquiler)
+        {
+            Id = id;
+            Nombre = nombre;
+            Marca = marca;
+            Procesador = procesador;
+            Cantidad = cantidad;
+            PrecioAlquiler = precioAlquiler;
+        }
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public int Cantidad { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public double PrecioAlquiler { get; set; }
+        [Required]
+        public string Procesador { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is PortatilParaAlquilerDTO dTO &&
+                   Id == dTO.Id && Nombre == dTO.Nombre &&
+                   Marca == dTO.Marca && Cantidad == dTO.Cantidad &&
+                   Procesador == dTO.Procesador &&
+                   PrecioAlquiler == dTO.PrecioAlquiler;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Nombre, Marca, Procesador, Cantidad, PrecioAlquiler);
+        }
+    }
+}
