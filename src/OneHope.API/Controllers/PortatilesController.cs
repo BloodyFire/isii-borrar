4b4cd0d38d88791ab4e1db4e6396a8cf59fec60c
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-11-03 20:59:56 +0100

    Creación del metodo get para alquilar portatiles en PortatilesController. #29

--- src/OneHope.API/Controllers/PortatilesController.cs
+++ src/OneHope.API/Controllers/PortatilesController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using OneHope.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+using OneHope.Shared.PortatilDTOs;
 
 namespace OneHope.API.Controllers
 {
@@ -16,6 +19,28 @@ namespace OneHope.API.Controllers
         {
             _context = context;
             _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<PortatilParaAlquilerDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult<PortatilParaAlquilerDTO>> GetPortatilesParaAlquiler(string? filtroMarca, string? filtroProcesador, string? filtroRam, int? filtroCantidadMinima, int? filtroCantidadMaxima)
+        {
+            var portatiles = await _context.Portatiles
+                .Where(portatil => ((filtroMarca == null || portatil.Marca.NombreMarca.Equals(filtroMarca)) &&
+                                    (filtroProcesador == null || portatil.Modelo.Contains(filtroProcesador)) &&
+                                    (filtroRam == null || portatil.Modelo.Contains(filtroRam)) &&
+                                    (filtroCantidadMinima == null || portatil.Cantidad >= filtroCantidadMinima) &&
+                                    (filtroCantidadMaxima == null || portatil.Cantidad <= filtroCantidadMaxima) 
+                                    ))
+                .Include(portatil => portatil.Ram)
+                .Include(portatil => portatil.Procesador)
+                .OrderBy(portatil => portatil.Cantidad)
+                .Select(portatil => new PortatilParaAlquilerDTO(portatil.Id, portatil.Nombre, portatil.Marca.NombreMarca, portatil.Procesador.ModeloProcesador, portatil.Cantidad, portatil.PrecioAlquiler))
+                .ToListAsync();
+
+            return Ok(portatiles);
         }
     }
 }
