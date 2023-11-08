09769ec3271a083d83b1088619004838c782a7a8
PAULA ORTEGA LÓPEZ <Paula.Ortega4@alu.uclm.es>
2023-11-08 17:11:28 +0100

    Creado controlador PortatilesController #24

--- src/OneHope.API/Controllers/PortatilesController.cs
+++ src/OneHope.API/Controllers/PortatilesController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using OneHope.API.Models;
+using Microsoft.EntityFrameworkCore;
+using OneHope.Shared.PortatilDTOs;
+using System.Net;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace OneHope.API.Controllers
 {
@@ -17,5 +22,39 @@ namespace OneHope.API.Controllers
             _context = context;
             _logger = logger;
         }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<PortatilesParaDevolverDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NoContent)]
+        public async Task<ActionResult<IList<PortatilesParaDevolverDTO>>> GetPortatilesParaDevolver(int? idCompra, DateTime? fecha, int? CustomerId)
+        {
+            
+                     
+
+            DateTime defaultDate = DateTime.Now.Date.AddDays(-30);
+           
+
+
+            IList<PortatilesParaDevolverDTO> portatiles = await _context.LineaCompra
+                    .Include(portatil => portatil.Compra)
+                    .Include(compra => compra.Portatil)
+                    .ThenInclude(marca => marca.Marca)
+                    .Where(portatil => (CustomerId == null || portatil.Compra.CustomerId.Equals(CustomerId)) &&
+                                       (idCompra == null || portatil.IdCompra.Equals(idCompra)) &&
+                                       ((fecha == null || portatil.Compra.FechaCompra.Equals(fecha)) &&
+                                        portatil.Compra.FechaCompra >= defaultDate)
+                                       )
+                    .OrderBy(portatil => portatil.Compra.FechaCompra)
+                    .Select(portatil => new PortatilesParaDevolverDTO(portatil.IdCompra, portatil.Portatil.Marca.NombreMarca, portatil.Cantidad,
+                    portatil.Compra.FechaCompra, portatil.Compra.Total)
+                     ).ToListAsync();
+            return Ok(portatiles);
+
+        }
+
     }
+
+
+
 }
