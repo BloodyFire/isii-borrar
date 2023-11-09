7b86949b0e2735c8f2420ff7966d5c2b2b30cddf
DANIEL TOMÁS MARTÍNEZ <Daniel.Tomas@alu.uclm.es>
2023-11-09 16:03:24 +0100

    Creado método get en PedidosController para obtener el detalle del pedido: GetPedido. #43

--- src/OneHope.API/Controllers/PedidosController.cs
+++ src/OneHope.API/Controllers/PedidosController.cs
@@ -77,9 +77,43 @@ namespace OneHope.API.Controllers
                 pedido.FechaPedido,
                 pedido.Comentarios);
 
-            //TODO: implementar GetPedido para que pueda devolver el resumen de pedido. #43
             return CreatedAtAction("GetPedido", new { id = pedido.Id }, detallePedido);
         }
 
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(DetallePedidoDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetPedido(int id)
+        {
+            if (_context.Pedidos == null)
+            {
+                _logger.LogError("Error: No existe la tabla de pedidos.");
+                return NotFound();
+            }
+
+            var pedidoDTO = await _context.Pedidos
+             .Where(pedido => pedido.Id == id)
+                 .Include(pedido => pedido.LineasPedido)
+                    .ThenInclude(lineaPedido => lineaPedido.Portatil)
+                        .ThenInclude(portatil => portatil.Marca)
+             .Select(pedido => new DetallePedidoDTO(pedido.Id, pedido.Direccion,
+                    pedido.LineasPedido
+                        .Select(pi => new LineaPedidoDTO(pi.Portatil.Id,
+                                pi.Portatil.Modelo,pi.PrecioUnitario,
+                                pi.Cantidad)).ToList<LineaPedidoDTO>(),
+                    pedido.CodigoEmpleado,
+                    pedido.TipoMetodoPago, pedido.FechaPedido, pedido.Comentarios))
+             .FirstOrDefaultAsync();
+
+
+            if (pedidoDTO == null)
+            {
+                _logger.LogError($"Error: El pedido con {id} no existe.");
+                return NotFound();
+            }
+
+            return Ok(pedidoDTO);
+        }
     }
 }
