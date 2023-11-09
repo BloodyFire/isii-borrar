b5e06ffe1c2933bf679e0f920a0428fcc1b8868a
DANIEL TOMÁS MARTÍNEZ <Daniel.Tomas@alu.uclm.es>
2023-11-09 18:15:00 +0100

    Simplificado los datos que se utilizan en las pruebas unitarias. #28 #42 #43 Eliminado UnitTest1 de ejemplo ya que no es una prueba real.

--- test/OneHope.UT/PedidosController_test/CreatePedido_test.cs
+++ test/OneHope.UT/PedidosController_test/CreatePedido_test.cs
@@ -4,33 +4,17 @@
     {
         public CreatePedido_test()
         {
-            var procesadores = new List<Procesador>() {
-                new Procesador("Intel I7 13700")
-            };
-            var rams = new List<Ram>() {
-                new Ram("8Gb")
-            };
-            var marcas = new List<Marca>() {
-                new Marca("HP"),
-                new Marca("DELL")
-            };
-            var proveedores = new List<Proveedor>() {
-                new Proveedor(1, "Proveedores S.L.", "44444444A", "Calle providia numero 75", "proveemos@proveedores.com", "600000000"),
-                new Proveedor(2, "Portatiles Mayorista", "12345678T", "Poligono del unicornio, avenida de la purpurina numero 7. Narnia 02000", "pormay@yahoorespuestas.com", "999555666")
-            };
+            var procesador = new Procesador("Intel I7 13700");
+            var ram = new Ram("8Gb");
+            var marca = new Marca("DELL");
+            var proveedor = new Proveedor(1, "Portatiles Mayorista", "12345678T", "Poligono del unicornio, avenida de la purpurina numero 7. Narnia 02000", "pormay@yahoorespuestas.com", "999555666");
 
-            var portatiles = new List<Portatil>()
-            {
-                new Portatil(id: 1, modelo: "HP-1151", procesador: procesadores[0], ram: rams[0], marca: marcas[0], nombre: "HP 486 del pleistoceno", precioCompra: 199.95, precioAlquiler: 6.66, precioCoste: 50.00, stock: 0, stockAlquilar: 5, proveedor: proveedores[0]),
-                new Portatil(2, "DELL-1244", procesadores[0], rams[0], marcas[1], "DELL R5 gama alta", 1999.95, 66.66, 500.00, 1, 1, proveedores[0]),
-                new Portatil(3, "ASUS-1362", procesadores[0], rams[0], marcas[0], "portatilote grandote marca asus perfecto para ir de camping.", 699.95, 23.33, 175.00, 5, 5, proveedores[1]),
-                new Portatil(4, "TOASTER-1421", procesadores[0], rams[0], marcas[1], "Portatil de 82 pulgadas. (Para empotrar en la pared)", 1299.95, 43.33, 325.00, 15, 6, proveedores[1])
-            };
-            _context.AddRange(procesadores);
-            _context.AddRange(rams);
-            _context.AddRange(marcas);
-            _context.AddRange(proveedores);
-            _context.AddRange(portatiles);
+            var portatil = new Portatil(1, "TOASTER-1421", procesador, ram, marca, "Portatil de 82 pulgadas. (Para empotrar en la pared)", 1299.95, 43.33, 325.00, 15, 6, proveedor);
+            _context.Add(procesador);
+            _context.Add(ram);
+            _context.Add(marca);
+            _context.Add(proveedor);
+            _context.Add(portatil);
             _context.SaveChanges(); //maybe async?
         }
 
@@ -38,20 +22,20 @@
         {
             PedidoParaCrearDTO pedidoSinComentarios = new PedidoParaCrearDTO("Calle falsa, 123. Ciudad de la piruleta.", new List<LineaPedidoDTO>(),
                 "Daniel.Tomas", TipoMetodoPago.Transferencia);
-            pedidoSinComentarios.LineasPedido.Add(new LineaPedidoDTO(4, "TOASTER-1421", 325.0, 1));
+            pedidoSinComentarios.LineasPedido.Add(new LineaPedidoDTO(1, "TOASTER-1421", 325.0, 1));
 
             PedidoParaCrearDTO pedidoConComentarios = new PedidoParaCrearDTO("Calle falsa, 123. Ciudad de la piruleta.",
                 new List<LineaPedidoDTO>(),
                  "Daniel.Tomas", TipoMetodoPago.Transferencia, "Tengo comentarios al respecto.");
-            pedidoConComentarios.LineasPedido.Add(new LineaPedidoDTO(4, "TOASTER-1421", 325.0, 1));
+            pedidoConComentarios.LineasPedido.Add(new LineaPedidoDTO(1, "TOASTER-1421", 325.0, 1));
 
             DetallePedidoDTO expectedPedidoSinComentarios = new DetallePedidoDTO(1, "Calle falsa, 123. Ciudad de la piruleta.", new List<LineaPedidoDTO>(),
                 "Daniel.Tomas", TipoMetodoPago.Transferencia, DateTime.Now);
-            expectedPedidoSinComentarios.LineasPedido.Add(new LineaPedidoDTO(4, "TOASTER-1421", 325.0, 1));
+            expectedPedidoSinComentarios.LineasPedido.Add(new LineaPedidoDTO(1, "TOASTER-1421", 325.0, 1));
 
             DetallePedidoDTO expectedPedidoConComentarios = new DetallePedidoDTO(1, "Calle falsa, 123. Ciudad de la piruleta.", new List<LineaPedidoDTO>(),
                 "Daniel.Tomas", TipoMetodoPago.Transferencia, DateTime.Now, "Tengo comentarios al respecto.");
-            expectedPedidoConComentarios.LineasPedido.Add(new LineaPedidoDTO(4, "TOASTER-1421", 325.0, 1));
+            expectedPedidoConComentarios.LineasPedido.Add(new LineaPedidoDTO(1, "TOASTER-1421", 325.0, 1));
 
             var allTests = new List<object[]>
             {
--- test/OneHope.UT/PortatilesController_test/GetPortatilesParaPedido_test.cs
+++ test/OneHope.UT/PortatilesController_test/GetPortatilesParaPedido_test.cs
@@ -4,12 +4,8 @@
     {
         public GetPortatilesParaPedido_test()
         {
-            var procesadores = new List<Procesador>() {
-                new Procesador("Intel I7 13700")
-            };
-            var rams = new List<Ram>() {
-                new Ram("8Gb")
-            };
+            var procesador = new Procesador("Intel I7 13700");
+            var ram = new Ram("8Gb");
             var marcas = new List<Marca>() {
                 new Marca("HP"),
                 new Marca("DELL")
@@ -20,13 +16,13 @@
             };
             var portatiles = new List<Portatil>()
             {
-                new Portatil(id: 1, modelo: "HP-1151", procesador: procesadores[0], ram: rams[0], marca: marcas[0], nombre: "HP 486 del pleistoceno", precioCompra: 199.95, precioAlquiler: 6.66, precioCoste: 50.00, stock: 0, stockAlquilar: 5, proveedor: proveedores[0]),
-                new Portatil(2, "DELL-1244", procesadores[0], rams[0], marcas[1], "DELL R5 gama alta", 1999.95, 66.66, 500.00, 1, 1, proveedores[0]),
-                new Portatil(3, "ASUS-1362", procesadores[0], rams[0], marcas[0], "portatilote grandote marca asus perfecto para ir de camping.", 699.95, 23.33, 175.00, 5, 5, proveedores[1]),
-                new Portatil(4, "TOASTER-1421", procesadores[0], rams[0], marcas[1], "Portatil de 82 pulgadas. (Para empotrar en la pared)", 1299.95, 43.33, 325.00, 15, 6, proveedores[1])
+                new Portatil(id: 1, modelo: "HP-1151", procesador: procesador, ram: ram, marca: marcas[0], nombre: "HP 486 del pleistoceno", precioCompra: 199.95, precioAlquiler: 6.66, precioCoste: 50.00, stock: 0, stockAlquilar: 5, proveedor: proveedores[0]),
+                new Portatil(2, "DELL-1244", procesador, ram, marcas[1], "DELL R5 gama alta", 1999.95, 66.66, 500.00, 1, 1, proveedores[0]),
+                new Portatil(3, "ASUS-1362", procesador, ram, marcas[0], "portatilote grandote marca asus perfecto para ir de camping.", 699.95, 23.33, 175.00, 5, 5, proveedores[1]),
+                new Portatil(4, "TOASTER-1421", procesador, ram, marcas[1], "Portatil de 82 pulgadas. (Para empotrar en la pared)", 1299.95, 43.33, 325.00, 15, 6, proveedores[1])
             };
-            _context.AddRange(procesadores);
-            _context.AddRange(rams);
+            _context.Add(procesador);
+            _context.Add(ram);
             _context.AddRange(marcas);
             _context.AddRange(proveedores);
             _context.AddRange(portatiles);
@@ -36,12 +32,8 @@
         [Fact]
         public async Task GetPortatilesParaPedido_ok_null()
         {
-            var procesadores = new List<Procesador>() {
-                new Procesador("Intel I7 13700")
-            };
-            var rams = new List<Ram>() {
-                new Ram("8Gb")
-            };
+            var procesador = new Procesador("Intel I7 13700");
+            var ram = new Ram("8Gb");
             var marcas = new List<Marca>() {
                 new Marca("HP"),
                 new Marca("DELL")
@@ -50,13 +42,12 @@
                 new Proveedor(1, "Proveedores S.L.", "44444444A", "Calle providia numero 75", "proveemos@proveedores.com", "600000000"),
                 new Proveedor(2, "Portatiles Mayorista", "12345678T", "Poligono del unicornio, avenida de la purpurina numero 7. Narnia 02000", "pormay@yahoorespuestas.com", "999555666")
             };
-
             var expectedPortatiles = new List<Portatil>()
             {
-                new Portatil(id: 1, modelo: "HP-1151", procesador: procesadores[0], ram: rams[0], marca: marcas[0], nombre: "HP 486 del pleistoceno", precioCompra: 199.95, precioAlquiler: 6.66, precioCoste: 50.00, stock: 0, stockAlquilar: 5, proveedor: proveedores[0]),
-                new Portatil(2, "DELL-1244", procesadores[0], rams[0], marcas[1], "DELL R5 gama alta", 1999.95, 66.66, 500.00, 1, 1, proveedores[0]),
-                new Portatil(3, "ASUS-1362", procesadores[0], rams[0], marcas[0], "portatilote grandote marca asus perfecto para ir de camping.", 699.95, 23.33, 175.00, 5, 5, proveedores[1]),
-                new Portatil(4, "TOASTER-1421", procesadores[0], rams[0], marcas[1], "Portatil de 82 pulgadas. (Para empotrar en la pared)", 1299.95, 43.33, 325.00, 15, 6, proveedores[1])
+                new Portatil(id: 1, modelo: "HP-1151", procesador: procesador, ram: ram, marca: marcas[0], nombre: "HP 486 del pleistoceno", precioCompra: 199.95, precioAlquiler: 6.66, precioCoste: 50.00, stock: 0, stockAlquilar: 5, proveedor: proveedores[0]),
+                new Portatil(2, "DELL-1244", procesador, ram, marcas[1], "DELL R5 gama alta", 1999.95, 66.66, 500.00, 1, 1, proveedores[0]),
+                new Portatil(3, "ASUS-1362", procesador, ram, marcas[0], "portatilote grandote marca asus perfecto para ir de camping.", 699.95, 23.33, 175.00, 5, 5, proveedores[1]),
+                new Portatil(4, "TOASTER-1421", procesador, ram, marcas[1], "Portatil de 82 pulgadas. (Para empotrar en la pared)", 1299.95, 43.33, 325.00, 15, 6, proveedores[1])
             }
             .OrderBy(p => p.Stock).Select(p => new PortatilParaPedidoDTO(p.Id, p.Modelo, p.Marca.NombreMarca, p.Stock, p.PrecioCoste, p.Proveedor.Nombre)).ToList();
 
