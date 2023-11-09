631ee7e3837e014b76eebf65bc7d5fac64672f67
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-11-09 20:43:17 +0100

    Creación del metodo Post del controlador de Alquileres. #29

--- src/OneHope.API/Controllers/AlquileresController.cs
+++ src/OneHope.API/Controllers/AlquileresController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using OneHope.API.Models;
 using OneHope.Shared.AlquilerDTOs;
@@ -15,6 +14,7 @@ namespace OneHope.API.Controllers
         private readonly ILogger<AlquileresController> _logger;
 
         private static int idLinea = 0;
+        private static int idAlquiler = 0;
 
         public AlquileresController(ApplicationDBContext context, ILogger<AlquileresController> logger)
         {
@@ -58,6 +58,103 @@ namespace OneHope.API.Controllers
             return Ok(alquilerDTO);
         }
 
-        
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(DetalleAlquilerDTO), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> CreateRental(AlquilerParaCrearDTO alquilerParaCrear)
+        {
+            if (_context.Alquileres == null)
+            {
+                _logger.LogError("Error: La tabla Alquileres no existe");
+                return Problem("Entity set 'ApplicationDBContext.Purchases'  is null.");
+            }
+
+            //any validation defined in PurchaseForCreate is checked before running the method so they don't have to be checked again
+            if (alquilerParaCrear.FechaInAlquiler <= DateTime.Today)
+                ModelState.AddModelError("FechaInAlquiler", "Error! Tu fecha de inicio de alquiler no puede empezar ni hoy ni antes");
+
+            if (alquilerParaCrear.FechaFinAlquiler >= alquilerParaCrear.FechaInAlquiler)
+                ModelState.AddModelError("FechaFinAlquiler&FechaInAlquiler", "Error! Tu fecha de fin de alquiler no puede acabar antes o el mismo dia que tu fecha de inicio");
+
+            if (alquilerParaCrear.LineasAlquiler.Count == 0)
+                ModelState.AddModelError("LineasAlquiler", "Error! Debes alquilar al menos un portatil");
+
+
+            if (ModelState.ErrorCount > 0)
+                return BadRequest(new ValidationProblemDetails(ModelState));
+
+
+            var portatilIDs = alquilerParaCrear.LineasAlquiler.Select(la => la.PortatilID).ToList<int>();
+
+            var movies = _context.Portatiles.Include(p => p.LineasAlquiler)
+                .ThenInclude(la => la.Alquiler)
+                .Where(p => portatilIDs.Contains(p.Id))
+
+                //we use an anonymous type https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/anonymous-types
+                .Select(p => new {
+                    p.Id,
+                    p.Modelo,
+                    p.StockAlquilar,
+                    //Elegimos los portatiles disponibles para alquilar
+                    NumeroDePortatilesAlquilados = p.LineasAlquiler.Count(la => la.Alquiler.FechaInAlquiler <= alquilerParaCrear.FechaFinAlquiler
+                            && la.Alquiler.FechaFinAlquiler >= alquilerParaCrear.FechaInAlquiler)
+                })
+                .ToList();
+
+
+
+            Alquiler alquiler = new Alquiler(idAlquiler, DateTime.Now, alquilerParaCrear.FechaInAlquiler, alquilerParaCrear.FechaFinAlquiler, 
+                alquilerParaCrear.Total, alquilerParaCrear.NombreCliente, alquilerParaCrear.ApellidosCliente, alquilerParaCrear.DireccionEnvio,
+                alquilerParaCrear.EmailCliente, (int)alquilerParaCrear.TelefonoCliente, (OneHope.API.Models.TipoMetodoPago)alquilerParaCrear.TipoMetodoPago,
+                new List<LineaAlquiler>());
+
+            idAlquiler++;
+
+            Portatil portatil;
+            foreach (var linea in alquilerParaCrear.LineasAlquiler)
+            {
+                portatil = await _context.Portatiles.FindAsync(linea.PortatilID);
+                //Check de que hay disponibles para alquilar
+                if ((portatil == null) || (portatil.StockAlquilar < linea.Cantidad))
+                {
+                    ModelState.AddModelError("RentalItems", $"Error! Portatil con id '{linea.PortatilID}' no puede ser alquilado desde {alquilerParaCrear.FechaInAlquiler.ToShortDateString()} hasta {alquilerParaCrear.FechaFinAlquiler.ToShortDateString()}");
+                }
+                else
+                {
+                    // rental does not exist in the database yet and does not have a valid Id, so we must relate rentalitem to the object rental
+                    alquiler.LineasAlquiler.Add(new LineaAlquiler(idLinea, linea.Cantidad, portatil, alquiler));
+                    idLinea++;
+                }
+            }
+
+            //if there is any problem because of the available quantity of movies or because the movie does not exist
+            if (ModelState.ErrorCount > 0)
+            {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+
+            _context.Add(alquiler);
+
+            try
+            {
+                //we store in the database both rental and its rentalitems
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                ModelState.AddModelError("Rental", $"Error! Hubo un error al guardar tu alquiler, intentalo mas tarde");
+            }
+
+            //it returns rentalDetail
+            var detalleAlquiler = new DetalleAlquilerDTO(alquiler.ID, alquiler.FechaAlquiler,
+                alquiler.EmailCliente, alquiler.NombreCliente, alquiler.ApellidosCliente,
+                alquiler.DireccionEnvio, alquiler.TelefonoCliente, alquilerParaCrear.TipoMetodoPago,
+                alquiler.FechaInAlquiler, alquiler.FechaFinAlquiler,
+                alquilerParaCrear.LineasAlquiler);
+
+            return CreatedAtAction("GetRental", new { id = alquiler.ID }, detalleAlquiler);
+        }
     }
 }
