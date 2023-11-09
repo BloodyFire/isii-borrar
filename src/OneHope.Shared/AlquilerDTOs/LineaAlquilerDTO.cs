5e8b0003075bb5324c28ade7148f3d3830415fca
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-11-09 18:29:49 +0100

    Creado el DTO de LineaAlquiler. #29

---
+++ src/OneHope.Shared/AlquilerDTOs/LineaAlquilerDTO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace OneHope.Shared.AlquilerDTOs
+{
+    public class LineaAlquilerDTO
+    {
+        public LineaAlquilerDTO(int id, int portatilID, double precioAlquiler, int cantidad) 
+        { 
+            Id = id;
+            PortatilID = portatilID;
+            PrecioAlquiler = precioAlquiler;
+            Cantidad = cantidad;
+        }
+
+        [JsonPropertyName("Id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("PortatilID")]
+        public int PortatilID { get; set; }
+
+        [Display(Name = "Precio Alquiler")]
+        [JsonPropertyName("PrecioAlquiler")]
+        public double PrecioAlquiler { get; set; }
+
+        [Required]
+        [JsonPropertyName("Cantidad")]
+        [Range(1, Double.MaxValue, ErrorMessage = "No se ha introducido una cantidad válida.")]
+        public int Cantidad { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is LineaAlquilerDTO model &&
+                Id == model.Id &&
+                PortatilID == model.PortatilID &&
+                PrecioAlquiler == model.PrecioAlquiler &&
+                Cantidad == model.Cantidad;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, PortatilID, PrecioAlquiler, Cantidad);
+        }
+    }
+}
