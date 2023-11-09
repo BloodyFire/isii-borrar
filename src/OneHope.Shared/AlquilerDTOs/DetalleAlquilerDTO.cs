65dcb87c225ab2e405523df94e3b05acf005a45c
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-11-09 19:19:26 +0100

    Se ha creado el DTO del DetalleAlquilerDTO. #29

---
+++ src/OneHope.Shared/AlquilerDTOs/DetalleAlquilerDTO.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OneHope.Shared.AlquilerDTOs
+{
+    public class DetalleAlquilerDTO : AlquilerParaCrearDTO
+    {
+        public DetalleAlquilerDTO(int id, DateTime fechaAlquiler, string emailCliente, string nombreCliente, string apellidosCliente,
+            string direccionEnvio, int? telefonoCliente, TipoMetodoPago tipoMetodoPago, DateTime fechaInAlquiler,
+            DateTime fechaFinAlquiler, IList<LineaAlquilerDTO> lineasAlquiler)
+            : base(fechaInAlquiler, fechaFinAlquiler, emailCliente,
+                  nombreCliente, apellidosCliente, direccionEnvio, telefonoCliente, 
+                  lineasAlquiler, tipoMetodoPago)
+        {
+            Id = id;
+            FechaAlquiler = fechaAlquiler;
+        }
+
+        [JsonPropertyName("Id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("RentalDate")]
+        public DateTime FechaAlquiler { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DetalleAlquilerDTO dTO &&
+                   base.Equals(obj) &&
+                   Total == dTO.Total &&
+                   Id == dTO.Id &&
+                   CompararFechas(FechaAlquiler, dTO.FechaAlquiler);
+        }
+    }
+}
