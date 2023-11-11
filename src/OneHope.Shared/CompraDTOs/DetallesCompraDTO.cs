e01b07a846f3c71dffc6b99d7c69eb4d1e40e498
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-11-11 14:13:13 +0100

    Solo he cambiado el nombre de la variables portatilesComprados a lineasCompra, he añadido un DisplayFormat a FechaCompra y he modificado el Equals.#44

--- src/OneHope.Shared/CompraDTOs/DetallesCompraDTO.cs
+++ src/OneHope.Shared/CompraDTOs/DetallesCompraDTO.cs
@@ -1,6 +1,7 @@
 ﻿using OneHope.Design.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -9,35 +10,31 @@ using System.Threading.Tasks;
 
 namespace OneHope.Shared.CompraDTOs
 {
-    public class DetallesCompraDTO : CompraPorCrearDTO
-    {
+    public class DetallesCompraDTO: CompraPorCrearDTO
+    { 
         public DetallesCompraDTO() { }
 
         public DetallesCompraDTO(int id, string nombreUsuario, string apellidosUsuario, 
-            string direccion, IList<CompraPortatilDTO> portatilesComprados, OneHope.Shared.TipoMetodoPago metodoPago,
-            DateTime fechaCompra) : 
-            base(direccion, portatilesComprados, nombreUsuario, apellidosUsuario, metodoPago)
+            string direccion, IList<LineaCompraDTO> lineasCompra, TipoMetodoPago metodoPago,
+            DateTime fechaCompra):
+            base(direccion, lineasCompra, nombreUsuario, apellidosUsuario, metodoPago)
             {
-                id = id;
-                fechaCompra = fechaCompra;
+                Id = id;
+                FechaCompra = fechaCompra;
+
             }
 
         [JsonPropertyName("id")]
         public int Id { get; set; }
 
         [JsonPropertyName("fechaCompra")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime FechaCompra { get; set; }
 
         public override bool Equals(object? obj)
         {
             return obj is DetallesCompraDTO dTO &&
                    base.Equals(obj) &&
-                   direccion == dTO.direccion &&
-                   EqualityComparer<IList<CompraPortatilDTO>>.Default.Equals(compraPortatiles, dTO.compraPortatiles) &&
-                   PrecioTotal == dTO.PrecioTotal &&
-                   nombreUsuario == dTO.nombreUsuario &&
-                   apellidosUsuario == dTO.apellidosUsuario &&
-                   MetodoPago == dTO.MetodoPago &&
                    Id == dTO.Id &&
                    FechaCompra == dTO.FechaCompra;
         }
