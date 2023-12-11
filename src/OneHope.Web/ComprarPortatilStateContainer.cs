8f47e3a8a2131cead994d15e8aa498a32e81c62e
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-12-11 08:47:57 +0100

    He añadido los setters y he creado un constructor para que dirección, nombreUsuario y apellidosUsuario no inicien a null. Dichos cambios afectan a la creación de una compra.#65

--- src/OneHope.Web/ComprarPortatilStateContainer.cs
+++ src/OneHope.Web/ComprarPortatilStateContainer.cs
@@ -16,7 +16,12 @@ namespace OneHope.Web {
             
         };
 
-        
+        public ComprarPortatilStateContainer()
+        {
+            Compra.DireccionEnvio = "";
+            Compra.NombreUsuario = "";
+            Compra.ApellidosUsuario = "";
+        }
         
          // Se añade un elemento al carrito de la compra.
         public void AddPortatilACompra(PortatilParaComprarDTO portatil, int? cantidad) {
@@ -24,74 +29,78 @@ namespace OneHope.Web {
             if (!Compra.LineaCompra.Any(li => li.PortatilID == portatil.Id)) {
                 int cant = (int)(cantidad == null ? 1 : cantidad);  // Si la cantidad es null lo añadimos con cantidad 1.
                 // Si pides directamente en la interfaz de usuario la cantidad, el último párametro en vez de ser 1 sería la propia cantidad.
-                Compra.LineaCompra.Add(LineaCompra(portatil.Id, portatil.Nombre, portatil.PrecioCompra, portatil.Marca, portatil.Procesador, portatil.Ram, portatil.Stock));
+                Compra.LineaCompra.Add(LineaCompra(portatil.Id, portatil.Nombre, portatil.PrecioCompra, portatil.Marca, portatil.Procesador, portatil.Ram, cant));
             }
         }
 
-        //Añade/elimina el artículo al carrito al pulsar en la casilla. *@
-        private void ToggleArticulo(PortatilParaComprarDTO portatil, ChangeEventArgs args)
-            {
-                bool _checked = (bool)args.Value;
-                if (_checked)
-                    AddPortatilACompra(portatil, 1);
-                else
-                    RemovePortatilACompra(portatil.Id);
-            }
+        public static LineaCompraDTO LineaCompra(int portatilID, string nombre, double precioUnitario,
+            string marca, string procesador, string ram, int cantidad)
+        {
+            LineaCompraDTO li = new LineaCompraDTO();
+            li.PortatilID = portatilID;
+            li.Nombre = nombre;
+            li.PrecioUnitario = precioUnitario;
+            li.Marca = marca;
+            li.Procesador = procesador;
+            li.Ram = ram;
+            li.Cantidad = cantidad;
 
-            // No sé por qué sólo se generar el constructor vacío para los DTOs, hay que investigar por qué.
-            // De momento como solución he creado este método auxiliar para emular el constructor.
-            public static LineaCompraDTO LineaCompra(int portatilID, string nombre, double precioUnitario,
-                string marca, string procesador, string ram, int cantidad)
-            {
-                LineaCompraDTO li = new LineaCompraDTO();
-                li.PortatilID = portatilID;
-                li.Nombre = nombre;
-                li.PrecioUnitario = precioUnitario;
-                li.Marca = marca;
-                li.Procesador = procesador;
-                li.Ram = ram;
-                li.Cantidad = cantidad;
-
-                return li;
-            }
+            return li;
+        }
         
-            // Se elimina un elemento del carrito de la compra.
+        // Se elimina un elemento del carrito de la compra.
 
-            public void RemovePortatilACompra(int portatilId) {
+        public void RemovePortatilACompra(int portatilId) {
 
-                LineaCompraDTO lineaFactura =
-                    Compra.LineaCompra.FirstOrDefault(li => li.PortatilID.Equals(portatilId));
-                Compra.LineaCompra.Remove(lineaFactura);
-            }
+            LineaCompraDTO lineaFactura =
+                Compra.LineaCompra.FirstOrDefault(li => li.PortatilID.Equals(portatilId));
+            Compra.LineaCompra.Remove(lineaFactura);
+        }
 
         
-            // Devuelve si el carrito está vacío o no.
-            public bool isEmpty() {
-                if (Compra.LineaCompra.Count == 0) return true;
-                else return false;
-            }
+        // Devuelve si el carrito está vacío o no.
+        public bool isEmpty() {
+            if (Compra.LineaCompra.Count == 0) return true;
+            else return false;
+        }
 
         
-            // Devuelve si el carrito incluye o no un artículo.
-            public bool includes(int id) {
-                if (Compra.LineaCompra.Any(li => li.PortatilID.Equals(id))) { return true; }
+        // Devuelve si el carrito incluye o no un artículo.
+        public bool includes(int id) {
+            if (Compra.LineaCompra.Any(li => li.PortatilID.Equals(id))) { return true; }
 
-                return false;
-            }
+            return false;
+        }
         
-            // Actualiza la cantidad que se quiere comprar del artículo cuyo id es 'id'.
-            public void UpdateCarrito(int id, int cantidad) {
-                LineaCompraDTO? portatil = Compra.LineaCompra.FirstOrDefault(li => li.PortatilID == id);
+        // Actualiza la cantidad que se quiere comprar del artículo cuyo id es 'id'.
+        public void UpdateCarrito(int id, int cantidad) {
+            LineaCompraDTO? portatil = Compra.LineaCompra.FirstOrDefault(li => li.PortatilID == id);
 
-                if (portatil != null) portatil.Cantidad = cantidad;
-            }
+            if (portatil != null) portatil.Cantidad = cantidad;
+        }
         
-            // Se ha terminado la compra, así que hay que vaciar el carrito..
-            public void FinalizarCompra() {
-                Compra = new CompraPorCrearDTO() {
-                    LineaCompra = new List<LineaCompraDTO>()
-                };
-            }
+        // Se ha terminado la compra, así que hay que vaciar el carrito..
+        public void FinalizarCompra() {
+            Compra = new CompraPorCrearDTO() {
+                LineaCompra = new List<LineaCompraDTO>()
+            };
+        }
+
+        public void SetDireccion(string direccion)
+        {
+            Compra.DireccionEnvio= direccion;
+        }
+
+        public void SetApellidos(string apellidos)
+        {
+            Compra.ApellidosUsuario = apellidos;
+        }
+
+        public void SetNombre(string nombre)
+        {
+            Compra.NombreUsuario = nombre;
+        }
+
     }
 
 
