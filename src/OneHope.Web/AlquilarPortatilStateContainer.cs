31a22a19f4594993a6d472497b67817c2cc598e9
JUAN FRANCISCO SÁNCHEZ TIMOTEO <JuanFco.Sanchez6@alu.uclm.es>
2023-12-11 21:18:45 +0100

    Cambios para la creacion de alquileres del container. #59, #71, #72

--- src/OneHope.Web/AlquilarPortatilStateContainer.cs
+++ src/OneHope.Web/AlquilarPortatilStateContainer.cs
@@ -12,7 +12,7 @@ namespace OneHope.Web
         public AlquilerParaCrearDTO Alquiler { get; 
                                             private set;  // El set es privado para evitar que código maligno pueda modificar la compra.
                                         } = new AlquilerParaCrearDTO() {
-            LineasPedido = new List<LineaAlquilerDTO>()
+            LineasAlquiler = new List<LineaAlquilerDTO>()
         };
 
         
@@ -20,21 +20,27 @@ namespace OneHope.Web
          // Se añade un elemento al carrito de la compra.
         public void AddArticuloACompra(PortatilParaAlquilerDTO portatil, int? cantidad) {
             // Se comprueba que ese artículo no se ha añadido ya al carrito.
-            if (!Alquiler.LineasPedido.Any(li => li.PortatilID == portatil.Id)) {
+            if (!Alquiler.LineasAlquiler.Any(li => li.PortatilID == portatil.Id)) {
                 int cant = (int)(cantidad == null ? 1 : cantidad);  // Si la cantidad es null lo añadimos con cantidad 1.
                 // Si pides directamente en la interfaz de usuario la cantidad, el último párametro en vez de ser 1 sería la propia cantidad.
-                Alquiler.LineasPedido.Add(LineaAlquiler(0, cant, portatil, Alquiler));
+                Alquiler.LineasAlquiler.Add(LineaAlquiler(portatil.Id, portatil.PrecioAlquiler, cant, portatil.Marca, portatil.Modelo,
+                    portatil.Procesador, portatil.Ram));
             }
         }
 
+
         // No sé por qué sólo se generar el constructor vacío para los DTOs, hay que investigar por qué.
         // De momento como solución he creado este método auxiliar para emular el constructor.
-        public static LineaAlquilerDTO LineaFactura(int portatilId, float precioAlquiler, int cantidad)
+        public static LineaAlquilerDTO LineaAlquiler(int portatilId, double precioAlquiler, int cantidad, string marca, string modelo, string procesador, string ram)
         {
             LineaAlquilerDTO li = new LineaAlquilerDTO();
             li.PortatilID = portatilId;
             li.PrecioAlquiler = precioAlquiler;
             li.Cantidad = cantidad;
+            li.Marca = marca;
+            li.Modelo = modelo;
+            li.Procesador = procesador;
+            li.Ram = ram;
 
             return li;
         }
@@ -44,36 +50,54 @@ namespace OneHope.Web
         public void RemoveArticuloACompra(int articuloId) {
 
             LineaAlquilerDTO lineaAlquiler =
-                Alquiler.LineasPedido.FirstOrDefault(li => li.PortatilID.Equals(articuloId));
-            Alquiler.LineasPedido.Remove(lineaAlquiler);
+                Alquiler.LineasAlquiler.FirstOrDefault(li => li.PortatilID.Equals(articuloId));
+            Alquiler.LineasAlquiler.Remove(lineaAlquiler);
         }
         
         // Devuelve si el carrito está vacío o no.
         public bool isEmpty() {
-            if (Alquiler.LineasPedido.Count == 0) return true;
+            if (Alquiler.LineasAlquiler.Count == 0) return true;
             else return false;
         }
 
         // Devuelve si el carrito incluye o no un artículo.
         public bool includes(int id) {
-            if (Alquiler.LineasPedido.Any(li => li.PortatilID.Equals(id))) { return true; }
+            if (Alquiler.LineasAlquiler.Any(li => li.PortatilID.Equals(id))) { return true; }
 
             return false;
         }
 
         // Actualiza la cantidad que se quiere comprar del artículo cuyo id es 'id'.
         public void UpdateCarrito(int id, int cantidad) {
-            LineaAlquilerDTO? portatil = Alquiler.LineasPedido.FirstOrDefault(li => li.PortatilID == id);
+            LineaAlquilerDTO? portatil = Alquiler.LineasAlquiler.FirstOrDefault(li => li.PortatilID == id);
 
             if (portatil != null) portatil.Cantidad = cantidad;
         }
 
-        
+        public void SetNombreCliente(string nombreCliente)
+        {
+            Alquiler.NombreCliente = nombreCliente;
+        }
+
+        public void SetApellidosCliente(string apellidosCliente)
+        {
+            Alquiler.ApellidosCliente = apellidosCliente;
+        }
+
+        public void SetEmailCliente(string emailCliente)
+        {
+            Alquiler.EmailCliente = emailCliente;
+        }
+
+        public void SetDireccionCliente(string direccionCliente)
+        {
+            Alquiler.Direccion = direccionCliente;
+        }
         
         // Se ha terminado la compra, así que hay que vaciar el carrito..
         public void FinalizarCompra() {
             Alquiler = new AlquilerParaCrearDTO() {
-                LineasPedido = new List<LineaAlquilerDTO>()
+                LineasAlquiler = new List<LineaAlquilerDTO>()
             };
         }
         
