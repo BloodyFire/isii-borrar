f9c254a99f04d6a5bf464debfdb9839b72736190
DANIEL TOMÁS MARTÍNEZ <Daniel.Tomas@alu.uclm.es>
2023-12-13 13:14:08 +0100

    Creadas las pruebas funcionales CU1_1 del flujo básico y del flujo básico con observaciones. #75

--- test/OneHope.UIT/CUReabastecerPortatiles/CUReabastecerPortatiles_UIT.cs
+++ test/OneHope.UIT/CUReabastecerPortatiles/CUReabastecerPortatiles_UIT.cs
@@ -62,6 +62,126 @@ namespace OneHope.UIT.CUReabastecerPortatiles
             Assert.Contains(expectedText, _driver.PageSource);
         }
 
+        [Fact]
+        public void CU1_1_Flujo_Basico()
+        {
+            // Arrange
+            var seleccionarPortatiles_PO = new SeleccionPortatilesPedido_PO(_driver, _output);
+            var crearPedido_PO = new CrearPedido_PO(_driver, _output);
+            var detallePedido_PO = new DetallePedido_PO(_driver, _output);
+
+            string expectedCabecera = "Resumen de la Pedido";
+            string expectedCodigoEmpleado = "<b>Codigo de empleado:</b> Daniel.Tomas";
+            string expectedMetodoPago = "<b>Método de Pago:</b> TarjetaCredito";
+            string expectedFecha = "<b>Fecha de pedido:</b> ";
+            string expectedDireccion = "<b>Dirección:</b> C/IS2, S/N\r\n02000\r\nAlbacete (Albacete)";
+            string unespectedField = "<b>Observaciones:</b>";
+
+            var expectedPortatiles = new List<string[]>
+            {
+                new string[] { "TOASTER", "TOASTER-4461", "850", "4", "3400" },
+                new string[] { "DELL", "DELL-5211", "1000", "2", "2000" }
+            };
+
+            var expectedTotal = new List<string[]>
+            {
+                new string[] { "Precio Total:", "5400" }
+            };
+
+            // Act
+            // Si has usado autenticación tendrás hacer login, en mi ejemplo no se usa.
+            Inicio();
+            // Navegar hasta la página de Reabastecer Portátiles.
+            Ir_A_ReabastecerPortatiles();
+            // Seleccionar los portátiles 4 y 10.
+            seleccionarPortatiles_PO.SeleccionarPortatiles(new List<string>() { "4", "10" });
+            // Pulsar el botón de pedir.
+            seleccionarPortatiles_PO.Pedir();
+            // Rellenar dirección de entrega de pago Tarjta.
+            crearPedido_PO.setDireccion($"C/IS2, S/N{Keys.Enter}02000{Keys.Enter}Albacete (Albacete)");
+            // Seleccionar como método de pago Tarjta.
+            crearPedido_PO.setMetodoPago("TarjetaCredito");
+            // Poner las cantidades.
+            crearPedido_PO.setCantidad("4", "4");
+            crearPedido_PO.setCantidad("10", "2");
+            // Pulsar pedir para finalizar la pedido.
+            expectedFecha += DateTime.Now.ToString("dd-MM-yyyy HH:mm").ToUpper();
+            crearPedido_PO.Pedir();
+            // Ahora se debe haber mostrado la página de detalle y puedo comprobar si todo ha ido bien.
+
+            // Assert
+            Assert.True(_driver.PageSource.Contains(expectedCabecera));
+            Assert.True(_driver.PageSource.Contains(expectedCodigoEmpleado));
+            Assert.True(_driver.PageSource.Contains(expectedMetodoPago));
+            Assert.True(_driver.PageSource.Contains(expectedFecha));
+            Assert.True(_driver.PageSource.Contains(expectedDireccion));
+            Assert.False(_driver.PageSource.Contains(unespectedField));
+            Assert.True(detallePedido_PO.CompruebaListaPortatiles(expectedPortatiles));
+            Assert.True(detallePedido_PO.CompruebaTotal(expectedTotal));
+
+        }
+
+        [Fact]
+        public void CU1_1_Flujo_Basico_Observaciones()
+        {
+            // Arrange
+            var seleccionarPortatiles_PO = new SeleccionPortatilesPedido_PO(_driver, _output);
+            var crearPedido_PO = new CrearPedido_PO(_driver, _output);
+            var detallePedido_PO = new DetallePedido_PO(_driver, _output);
+
+            string expectedCabecera = "Resumen de la Pedido";
+            string expectedCodigoEmpleado = "<b>Codigo de empleado:</b> Daniel.Tomas";
+            string expectedMetodoPago = "<b>Método de Pago:</b> TarjetaCredito";
+            string expectedFecha = "<b>Fecha de pedido:</b> ";
+            string expectedDireccion = "<b>Dirección:</b> C/IS2, S/N\r\n02000\r\nAlbacete (Albacete)";
+            string expectedObservaciones = "<b>Observaciones:</b> Llamar a Antonio cuando llegue el pedido";
+
+            var expectedPortatiles = new List<string[]>
+            {
+                new string[] { "TOASTER", "TOASTER-4461", "850", "4", "3400" },
+                new string[] { "DELL", "DELL-5211", "1000", "2", "2000" }
+            };
+
+            var expectedTotal = new List<string[]>
+            {
+                new string[] { "Precio Total:", "5400" }
+            };
+
+            // Act
+            // Si has usado autenticación tendrás hacer login, en mi ejemplo no se usa.
+            Inicio();
+            // Navegar hasta la página de Reabastecer Portátiles.
+            Ir_A_ReabastecerPortatiles();
+            // Seleccionar los portátiles 4 y 10.
+            seleccionarPortatiles_PO.SeleccionarPortatiles(new List<string>() { "4", "10" });
+            // Pulsar el botón de pedir.
+            seleccionarPortatiles_PO.Pedir();
+            // Rellenar dirección de entrega.
+            crearPedido_PO.setDireccion($"C/IS2, S/N{Keys.Enter}02000{Keys.Enter}Albacete (Albacete)");
+            // Seleccionar como método de pago Tarjta.
+            crearPedido_PO.setMetodoPago("TarjetaCredito");
+            // Rellenar Observaciones.
+            crearPedido_PO.setComentarios("Llamar a Antonio cuando llegue el pedido");
+            // Poner las cantidades.
+            crearPedido_PO.setCantidad("4", "4");
+            crearPedido_PO.setCantidad("10", "2");
+            // Pulsar pedir para finalizar la pedido.
+            expectedFecha += DateTime.Now.ToString("dd-MM-yyyy HH:mm").ToUpper();
+            crearPedido_PO.Pedir();
+
+            // Ahora se debe haber mostrado la página de detalle y puedo comprobar si todo ha ido bien.
+
+            // Assert
+            Assert.True(_driver.PageSource.Contains(expectedCabecera));
+            Assert.True(_driver.PageSource.Contains(expectedCodigoEmpleado));
+            Assert.True(_driver.PageSource.Contains(expectedMetodoPago));
+            Assert.True(_driver.PageSource.Contains(expectedFecha));
+            Assert.True(_driver.PageSource.Contains(expectedDireccion));
+            Assert.True(_driver.PageSource.Contains(expectedObservaciones));
+            Assert.True(detallePedido_PO.CompruebaListaPortatiles(expectedPortatiles));
+            Assert.True(detallePedido_PO.CompruebaTotal(expectedTotal));
+
+        }
 
 
 
