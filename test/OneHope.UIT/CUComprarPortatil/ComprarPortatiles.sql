c896da5ae3cc916c08cbca7ef275636a21185c10
JOSE GARCÍA MOLINA <Jose.Garcia27@alu.uclm.es>
2023-12-17 18:46:16 +0100

    Base de datos necesaria para pasar las pruebas UIT del Caso de Uso Comprar Portátiles.#78

---
+++ test/OneHope.UIT/CUComprarPortatil/ComprarPortatiles.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [dbo].[Portatiles] ON
+INSERT INTO [dbo].[Portatiles] ([Id], [Modelo], [PrecioCompra], [RamId], [PrecioAlquiler], [MarcaId], [Nombre], [PrecioCoste], [ProcesadorId], [Stock], [StockAlquilar], [ProveedorId]) VALUES (1, N'HP-1151', 199.95, 1, 6.66, 1, N'HP 486 del pleistoceno', 100, 5, 12, 5, 1)
+INSERT INTO [dbo].[Portatiles] ([Id], [Modelo], [PrecioCompra], [RamId], [PrecioAlquiler], [MarcaId], [Nombre], [PrecioCoste], [ProcesadorId], [Stock], [StockAlquilar], [ProveedorId]) VALUES (2, N'DELL-2222', 499.95, 2, 1, 2, N'DELL I5 para ofimatica', 250, 2, 28, 7, 2)
+INSERT INTO [dbo].[Portatiles] ([Id], [Modelo], [PrecioCompra], [RamId], [PrecioAlquiler], [MarcaId], [Nombre], [PrecioCoste], [ProcesadorId], [Stock], [StockAlquilar], [ProveedorId]) VALUES (3, N'ASUS-3314', 2299.95, 3, 76.66, 3, N'ASUS PRO STATION 3000', 1150, 1, 16, 5, 4)
+INSERT INTO [dbo].[Portatiles] ([Id], [Modelo], [PrecioCompra], [RamId], [PrecioAlquiler], [MarcaId], [Nombre], [PrecioCoste], [ProcesadorId], [Stock], [StockAlquilar], [ProveedorId]) VALUES (44, N'TOASTER-4422', 199.95, 4, 6.66, 4, N'All-In-One de 5 pulgadas', 100, 2, 20, 6, 2)
+SET IDENTITY_INSERT [dbo].[Portatiles] OFF
